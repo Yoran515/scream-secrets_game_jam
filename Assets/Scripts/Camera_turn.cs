90e167862d4814589c367a8aa684714db9154655
Yoran515 <yoran.vis2004@gmail.com>
2024-08-24 20:01:12 +0200

    test

---
+++ Assets/Scripts/Camera_turn.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_turn : MonoBehaviour
+{
+
+    public Camera _camera;
+    public Transform cameraRotation;
+    public float rotationSpeed = 1.0f; 
+
+    private Quaternion targetRotation; 
+    private bool isTurning = false;
+
+    // Start is called before the first frame update
+    void Start()
+    { 
+        targetRotation = cameraRotation.rotation;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        if (Input.GetKeyDown(KeyCode.A) && !isTurning&& cameraRotation.rotation.y > -90f)
+        {
+            targetRotation = Quaternion.Euler(cameraRotation.eulerAngles + new Vector3(0, -90, 0));
+
+            isTurning = true;
+            _camera.fieldOfView = 52.2f;
+        }
+        if (Input.GetKeyDown(KeyCode.D) && !isTurning && cameraRotation.rotation.y < 90f)
+        {
+            targetRotation = Quaternion.Euler(cameraRotation.eulerAngles + new Vector3(0, 90, 0)); 
+            isTurning = true;
+            _camera.fieldOfView = 45.2f;
+        }
+
+        if (isTurning)
+        {
+            cameraRotation.rotation = Quaternion.Lerp(cameraRotation.rotation, targetRotation, Time.deltaTime * rotationSpeed);
+
+           
+            if (Quaternion.Angle(cameraRotation.rotation, targetRotation) < 0.1f)
+            {
+                cameraRotation.rotation = targetRotation; 
+                isTurning = false; 
+            }
+        }
+    }
+}
